c2a87ff1b88a5f16ed18a64d04a6dc522fe29bea
Mark Volkov <markasch67@gmail.com>
2024-12-06 15:49:48 +0300

    added get_date()

--- src/widget.py
+++ src/widget.py
@@ -7,3 +7,8 @@ def mask_account_card(data: str) -> str:
         return f"Счет {get_mask_account(data[-20:])}"
     else:
         return f"{data[:-17]} {get_mask_card_number(data[-16:])}"
+
+
+def get_date(date: str) -> str:
+    """Функция возвращает дату в определённом формате"""
+    return f"{date[8:10]}.{date[5:7]}.{date[:4]}"
