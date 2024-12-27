dd5751b339629c94a7b8dde6fa7cfe50e038d03c
Mark Volkov <markasch67@gmail.com>
2024-12-27 23:10:08 +0300

    Using fixtures

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -2,48 +2,27 @@ import pytest
 
 from src.processing import filter_by_state, sort_by_date
 
-
-@pytest.mark.parametrize(
-    "list_of_transactions, state, expected_list",
-    [
-        (
-            [
+@pytest.fixture
+def list_of_transactions():
+    return [
                 {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
                 {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
                 {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
                 {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ],
-            "EXECUTED",
-            [
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            ],
-        )
-    ],
-)
-def test_filter_by_state(list_of_transactions, state, expected_list):
-    assert filter_by_state(list_of_transactions, state) == expected_list
+            ]
 
 
-@pytest.mark.parametrize(
-    "list_of_transactions, descending, expected_list",
-    [
-        (
-            [
+def test_filter_by_state(list_of_transactions):
+    assert filter_by_state(list_of_transactions, "EXECUTED") == [
                 {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
                 {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ],
-            True,
-            [
+            ]
+
+
+def test_sort_by_date(list_of_transactions):
+    assert sort_by_date(list_of_transactions) == [
                 {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
                 {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
                 {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
                 {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            ],
-        )
-    ],
-)
-def test_sort_by_date(list_of_transactions, descending, expected_list):
-    assert sort_by_date(list_of_transactions, descending) == expected_list
+            ]
