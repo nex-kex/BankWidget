deef280e97f82cdae169c0300dab5fa8486f646b
Mark Volkov <markasch67@gmail.com>
2025-01-02 21:44:18 +0300

    Added tests fro generators

--- src/generators.py
+++ src/generators.py
@@ -7,15 +7,11 @@ def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[dict
         if transaction["operationAmount"]["currency"]["code"] == currency:
             yield transaction
 
-    # ошибка - окончание списка
 
-
-def transaction_descriptions(transactions: list[dict], currency: str) -> Iterator[str]:
+def transaction_descriptions(transactions: list[dict]) -> Iterator[str]:
     """Генератор, который возвращает описание каждой операции по очереди"""
     for transaction in transactions:
         yield transaction["description"]
-
-    # ошибка - окончание списка
 
 
 def card_number_generator(start: int, finish: int) -> Iterator[str]:
@@ -26,6 +22,6 @@ def card_number_generator(start: int, finish: int) -> Iterator[str]:
         str_card_number = str(int_card_number)
         while len(str_card_number) != 16:
             str_card_number = "0" + str_card_number
-        card_number = f"{str_card_number[:5]} {str_card_number[4:9]} {str_card_number[8:13]} {str_card_number[12:]}"
+        card_number = f"{str_card_number[:4]} {str_card_number[4:8]} {str_card_number[8:12]} {str_card_number[12:]}"
         yield card_number
         int_card_number += 1
