73e8a3e36b1fa3746494c42cfd6076f78ed56739
Mark Volkov <markasch67@gmail.com>
2025-01-02 21:56:51 +0300

    Updated README.md

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ def list_of_transactions():
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}
     ]
 
 
@@ -21,7 +21,7 @@ def transactions():
             "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
             "description": "Перевод организации",
             "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702",
+            "to": "Счет 11776614605963066702"
         },
         {
             "id": 142264268,
@@ -30,7 +30,7 @@ def transactions():
             "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
             "description": "Перевод со счета на счет",
             "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188",
+            "to": "Счет 75651667383060284188"
         },
         {
             "id": 873106923,
@@ -39,7 +39,7 @@ def transactions():
             "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
             "description": "Перевод со счета на счет",
             "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160",
+            "to": "Счет 74489636417521191160"
         },
         {
             "id": 895315941,
@@ -48,7 +48,7 @@ def transactions():
             "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
             "description": "Перевод с карты на карту",
             "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229",
+            "to": "Visa Platinum 8990922113665229"
         },
         {
             "id": 594226727,
@@ -57,6 +57,6 @@ def transactions():
             "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
             "description": "Перевод организации",
             "from": "Visa Platinum 1246377376343588",
-            "to": "Счет 14211924144426031657",
-        },
+            "to": "Счет 14211924144426031657"
+        }
     ]
