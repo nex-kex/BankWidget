6c2c3eb083cd5899efe2fd0b9b7baf7258130e99
Mark Volkov <markasch67@gmail.com>
2025-01-27 18:07:01 +0300

    Added logging to masks amd utils modules

--- src/masks.py
+++ src/masks.py
@@ -1,11 +1,36 @@
 from typing import Union
+import logging
+
+
+log_path = "./logs/masks.log"
+
+# Устраняет ошибку отсутствия файла при импорте модуля
+if __name__ == "__main__":
+    log_path = "." + log_path
+
+
+logger = logging.getLogger("masks")
+file_handler = logging.FileHandler(log_path, "w", encoding="utf-8")
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def get_mask_card_number(card_number: Union[int, str]) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску"""
 
-    # Номер карты в строковом формате
-    s: str = str(card_number)
+    try:
+        # Номер карты в строковом формате
+        s: str = str(card_number)
+
+        if len(s) != 16:
+            logger.warning(f"Masked a card with wrong length: {len(s)} when 16 is required.")
+        else:
+            logger.info("Successfully masked card number.")
+
+    except Exception as ex:
+        logger.error(f"Exception occurred: {ex}")
 
     return f"{s[0:4]} {s[4:6]}** **** {s[-4:]}"
 
@@ -13,7 +38,16 @@ def get_mask_card_number(card_number: Union[int, str]) -> str:
 def get_mask_account(account_number: Union[int, str]) -> str:
     """Функция принимает на вход номер счёта и возвращает его маску"""
 
-    # Номер счёта в строковом формате
-    s: str = str(account_number)
+    try:
+        # Номер счёта в строковом формате
+        s: str = str(account_number)
+
+        if len(s) != 20:
+            logger.warning(f"Masked an account with wrong length: {len(s)} when 20 is required.")
+        else:
+            logger.info("Successfully masked account number.")
+
+    except Exception as ex:
+        logger.error(f"Exception occurred: {ex}")
 
     return f"**{s[-4:]}"
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,21 @@
 import json
-
 from src.external_api import convert_to_rub
+import logging
+
+
+log_path = "./logs/utils.log"
+
+# Устраняет ошибку отсутствия файла при импорте модуля
+if __name__ == "__main__":
+    log_path = "." + log_path
+
+
+logger = logging.getLogger("utils")
+file_handler = logging.FileHandler(log_path, "w", encoding="utf-8")
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def get_transactions_info(file_path: str) -> list[dict]:
@@ -8,22 +23,42 @@ def get_transactions_info(file_path: str) -> list[dict]:
 
     try:
         with open(file_path, encoding="utf-8") as f:
+            logger.info(f"Loading transactions from {file_path}...")
             transactions = json.load(f)
-    except FileNotFoundError:
+
+    except FileNotFoundError as ex:
+        logger.error(f"Exception occurred: {ex}")
         return []
-    except json.JSONDecodeError:
+
+    except json.JSONDecodeError as ex:
+        logger.error(f"Exception occurred: {ex}")
         return []
+
+    except Exception as ex:
+        logger.error(f"Exception occurred: {ex}")
 
     return transactions
 
 
 def get_transaction_amount(transaction: dict) -> float:
     """Возвращает сумму транзакции в рублях"""
-    if transaction.get("operationAmount"):
-        currency = transaction["operationAmount"]["currency"]["code"]
-        if currency == "RUB":
-            return transaction["operationAmount"]["amount"]
+
+    try:
+        if transaction.get("operationAmount"):
+            currency = transaction["operationAmount"]["currency"]["code"]
+
+            if currency == "RUB":
+                logger.info("Transaction's amount in RUB.")
+                return transaction["operationAmount"]["amount"]
+
+            else:
+                logger.info("Converting transaction's amount to RUB...")
+                return convert_to_rub(transaction["operationAmount"]["amount"], currency)
+
         else:
-            return convert_to_rub(transaction["operationAmount"]["amount"], currency)
-    else:
+            logger.warning("Could not determine transaction's amount.")
+            return 0
+
+    except Exception as ex:
+        logger.error(f"Exception occurred: {ex}")
         return 0
